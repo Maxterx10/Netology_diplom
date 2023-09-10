f2f2d70a8fe0f734187c2c91550eb17547e8d27a
”Maxterx10” <maxterx10@mail.ru>
2023-09-10 23:47:46 +0300

    Add diplom files

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,16 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.4.0"
+  hashes = [
+    "h1:R97FTYETo88sT2VHfMgkPU3lzCsZLunPftjSI5vfKe8=",
+  ]
+}
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.96.1"
+  hashes = [
+    "h1:B5sx5nZjjV2E/QvpT+PGtySmxHObZkec13zg8xW0+94=",
+  ]
+}
---
+++ terraform/network.tf
@@ -0,0 +1,39 @@
+#Network-1
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network-1"
+}
+
+#Subnet-1
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet-1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  route_table_id = yandex_vpc_route_table.rt.id
+}
+
+resource "yandex_vpc_gateway" "nat_gateway" {
+  name = "nat-gateway"
+  shared_egress_gateway {}
+}
+
+resource "yandex_vpc_route_table" "rt" {
+  name       = "rt"
+  network_id = yandex_vpc_network.network-1.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id         = yandex_vpc_gateway.nat_gateway.id
+  }
+}
+
+#Subnet-2
+
+resource "yandex_vpc_subnet" "subnet-2" {
+  name           = "subnet-2"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.11.0/24"]
+}
